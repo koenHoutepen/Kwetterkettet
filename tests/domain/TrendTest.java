bb3fe0df3b780c6ee3d4a572878a6a4ace4da185
koenHoutepen <koen_houtepen@hotmail.com>
2018-02-27 12:19:58 +0100

    tweede commit

--- tests/domain/TrendTest.java
+++ tests/domain/TrendTest.java
@@ -5,7 +5,7 @@ import junit.framework.TestCase;
 public class TrendTest extends TestCase {
 
     public void setUp() throws Exception {
-        super.setUp();
+        super.setUp(); // allemaal leuk
     }
 
     public void tearDown() throws Exception {
